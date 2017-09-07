08a0e80a24b94396ea85c91864d16bc946bfc9e5
Augustus Chang <augustus.chang@rutgers.edu>
2017-09-06 23:58:26 -0400

    style input bar

--- react_app/components/DoShareApp.js
+++ react_app/components/DoShareApp.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MyAppBar from './AppBar';
 import Greeting from './Greeting';
+import InputBar from './InputBar';
 
 export default () => (
   <div>
     <MyAppBar />
     <Greeting />
+    <InputBar />
     <p> Input Bar goes here </p>  {/* InputBar */}
     <div> {/* ToDoList */}
        ToDoList goes here
--- react_app/components/InputBar.js
+++ react_app/components/InputBar.js
@@ -1,1 +1,24 @@
 import React from 'react';
+import { Input, Button } from 'react-toolbox';
+import styles from '../../styles/components/InputBar.css';
+
+
+export default class InputBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { task: '' };
+  }
+
+  handleTaskChange(task) {
+    this.setState({ task });
+  }
+
+  render() {
+    return (
+      <div className={styles.input_bar}>
+        <Input className={styles.bar} type="text" label="Task" value={this.state.task} onChange={val => this.handleTaskChange(val)} maxLength={25} />
+        <Button className={styles.button} icon="list" label="Add" raised primary />
+      </div>
+    );
+  }
+}
